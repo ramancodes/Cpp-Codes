e6a6979b8a158b8878e90877aca31144c672af66
Raman Manjhi <90102589+ramancodes@users.noreply.github.com>
2023-12-16 11:59:52 +0530

    Update login.cpp

--- login.cpp
+++ login.cpp
@@ -7,30 +7,42 @@
 
 using namespace std;
 
-bool registerUser(const string& username, const string& password) {
-    // Read existing user credentials from file
-    ifstream userFile("users.txt");
-    if (!userFile.is_open()) {
-        cerr << "Error opening user file." << endl;
-        return false;
-    }
+bool registerUser(const string &username, const string &password)
+{
 
     map<string, string> users;
 
-    // Populate map with username-password pairs from the file
-    string line;
-    while (getline(userFile, line)) {
-        istringstream iss(line);
-        string storedUsername, storedPassword;
-        if (iss >> storedUsername >> storedPassword) {
-            users[storedUsername] = storedPassword;
-        }
-    }
+    ifstream file;
+    file.open("users.txt");
 
-    // Check if the username already exists
-    if (users.find(username) != users.end()) {
-        cout << "Registration failed. Username already exists." << endl;
-        return false;
+    if (file)
+    {
+        // Read existing user credentials from file
+        ifstream userFile("users.txt");
+        if (!userFile.is_open())
+        {
+            cerr << "Error opening user file." << endl;
+            return false;
+        }
+
+        // Populate map with username-password pairs from the file
+        string line;
+        while (getline(userFile, line))
+        {
+            istringstream iss(line);
+            string storedUsername, storedPassword;
+            if (iss >> storedUsername >> storedPassword)
+            {
+                users[storedUsername] = storedPassword;
+            }
+        }
+
+        // Check if the username already exists
+        if (users.find(username) != users.end())
+        {
+            cout << "Registration failed. Username already exists." << endl;
+            return false;
+        }
     }
 
     // Register the new user
@@ -38,12 +50,14 @@ bool registerUser(const string& username, const string& password) {
 
     // Write the updated user credentials to the file
     ofstream outFile("users.txt");
-    if (!outFile.is_open()) {
+    if (!outFile.is_open())
+    {
         cerr << "Error opening user file for writing." << endl;
         return false;
     }
 
-    for (const auto& entry : users) {
+    for (const auto &entry : users)
+    {
         outFile << entry.first << " " << entry.second << endl;
     }
 
@@ -51,10 +65,12 @@ bool registerUser(const string& username, const string& password) {
     return true;
 }
 
-bool authenticateUser(const string& username, const string& password) {
+bool authenticateUser(const string &username, const string &password)
+{
     // Read user credentials from file
     ifstream userFile("users.txt");
-    if (!userFile.is_open()) {
+    if (!userFile.is_open())
+    {
         cerr << "Error opening user file." << endl;
         return false;
     }
@@ -63,33 +79,40 @@ bool authenticateUser(const string& username, const string& password) {
 
     // Populate map with username-password pairs from the file
     string line;
-    while (getline(userFile, line)) {
+    while (getline(userFile, line))
+    {
         istringstream iss(line);
         string storedUsername, storedPassword;
-        if (iss >> storedUsername >> storedPassword) {
+        if (iss >> storedUsername >> storedPassword)
+        {
             users[storedUsername] = storedPassword;
         }
     }
 
     // Check if the provided username exists and the password matches
     auto it = users.find(username);
-    if (it != users.end() && it->second == password) {
+    if (it != users.end() && it->second == password)
+    {
         cout << "Authentication successful!" << endl;
         return true;
-    } else {
+    }
+    else
+    {
         cout << "Authentication failed. Invalid username or password." << endl;
         return false;
     }
 }
 
-int main() {
+int main()
+{
     int choice;
     string username, password;
 
     cout << "1. Register\n2. Authenticate\nEnter choice (1 or 2): ";
     cin >> choice;
 
-    if (choice == 1) {
+    if (choice == 1)
+    {
         cout << "Enter username: ";
         cin >> username;
 
@@ -97,7 +120,9 @@ int main() {
         cin >> password;
 
         registerUser(username, password);
-    } else if (choice == 2) {
+    }
+    else if (choice == 2)
+    {
         cout << "Enter username: ";
         cin >> username;
 
@@ -105,7 +130,9 @@ int main() {
         cin >> password;
 
         authenticateUser(username, password);
-    } else {
+    }
+    else
+    {
         cout << "Invalid choice." << endl;
     }
 
