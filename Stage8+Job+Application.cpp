40c5d2f35b2b930d09a7f24751f4e42b84be73aa
Raman Manjhi <90102589+ramancodes@users.noreply.github.com>
2023-12-19 15:50:05 +0530

    Update Stage8+Job+Application.cpp

--- Stage8+Job+Application.cpp
+++ Stage8+Job+Application.cpp
@@ -6,6 +6,176 @@
 
 using namespace std;
 
+// registration class
+class Registration
+{
+private:
+    int choice;
+    string user, pass;
+
+    // register new user
+    bool registerUser(const string &username, const string &password, const int &reg_type)
+    {
+        string filepath;
+        string users;
+        switch (reg_type)
+        {
+        case 1:
+            filepath = "jobseekers_credentials.txt";
+            break;
+
+        case 2:
+            filepath = "employers_credentials.txt";
+            break;
+
+        default:
+            break;
+        }
+
+        ifstream file;
+        file.open(filepath);
+
+        // checks if the file exists
+        if (file)
+        {
+            // Read existing user credentials from file
+            ifstream userFile(filepath);
+            if (!userFile.is_open())
+            {
+                cerr << "\n\t\t\t\t\tError opening file." << endl
+                     << endl;
+                return false;
+            }
+
+            // Populate map with username-password pairs from the file
+            string line;
+            while (getline(userFile, line))
+            {
+                size_t pos = line.find("|");
+                string id = line.substr(0, pos);
+                if (id == username)
+                {
+                    cout << "\n\t\t\t\t\tUsername already exists." << endl
+                         << endl;
+                    return false;
+                }
+            }
+        }
+
+        // Register the new user mapping
+        users = username + "|" + password;
+
+        ofstream outFile(filepath);
+        if (outFile.is_open())
+        {
+            outFile << users << endl;
+            cout << "\n\t\t\t\t\tProfile saved successfully!" << endl;
+            return true;
+        }
+
+        cerr << "\n\t\t\t\t\tError opening user file for writing." << endl
+             << endl;
+        return false;
+    }
+
+    // take input (username & password) from the user
+    void user_registration_input(int reg_type)
+    {
+        cout << "\t\t\t\t\tEnter username: ";
+        cin >> user;
+
+        cout << "\t\t\t\t\tEnter password: ";
+        cin >> pass;
+
+        if (registerUser(user, pass, reg_type))
+        {
+            char c;
+            cout << "\n\t\t\t\t\tRegistration successful!" << endl
+                 << endl;
+            cout << "\t\t\t\t\tLogin To Continue." << endl
+                 << endl;
+
+            cout << "\t\t\t\t\tDo you want to go to login screen? \n\t\t\t\t\t'Y/y' for yes 'N/n' for no"
+                 << "\n\n\t\t\t\t\tEnter: ";
+            cin >> c;
+            if (c == 'Y' || c == 'y')
+            {
+                // redirect to the login screen
+                cout << "\n\t\t\t\t\tHere You Can Login To the Application";
+                cout << "\n\n\t\t\t\t\tPress Enter Key To Continue ";
+                getch();
+            }
+        }
+        else
+        {
+            cout << "\t\t\t\t\tRegistration failed." << endl;
+            cout << "\t\t\t\t\tPress Enter Key To Continue ";
+            getch();
+        }
+    }
+
+    // register new job seeker
+    void jobseeker_registration(int reg_type)
+    {
+        system("CLS");
+        cout << endl
+             << endl
+             << "\t\t\t\t\t---------------------------------------" << endl
+             << "\t\t\t\t\t ::Register As A Job Seeker::" << endl
+             << "\t\t\t\t\t---------------------------------------" << endl
+             << endl;
+        user_registration_input(reg_type);
+    }
+
+    // register new employer
+    void employer_registration(int reg_type)
+    {
+        system("CLS");
+        cout << endl
+             << endl
+             << "\t\t\t\t\t---------------------------------------" << endl
+             << "\t\t\t\t\t\t::Register As An Employer::" << endl
+             << "\t\t\t\t\t---------------------------------------" << endl
+             << endl;
+        user_registration_input(reg_type);
+    }
+
+public:
+    // registration calling function
+    void registration_call()
+    {
+        system("CLS");
+        cout << endl
+             << endl
+             << "\t\t\t\t\t---------------------------------------" << endl
+             << "\t\t\t\t\t ::Register To Job Application System::" << endl
+             << "\t\t\t\t\t---------------------------------------" << endl
+             << endl
+             << "\t\t\t\t\t1. Job Seeker" << endl
+             << "\t\t\t\t\t2. Employer" << endl
+             << "\t\t\t\t\t0. Home Screen" << endl
+             << endl
+             << "\t\t\t\t\tEnter: ";
+        cin >> choice;
+
+        // choose whom to register
+        switch (choice)
+        {
+        case 1:
+            jobseeker_registration(choice);
+            break;
+
+        case 2:
+            employer_registration(choice);
+            break;
+
+        default:
+            break;
+        }
+    }
+
+};
+
 class JobApplication
 {
 protected:
@@ -168,18 +338,6 @@ protected:
                     cout << "\n\n\t\t\t\t\tJob ID: " << jobId << endl;
                     cout << "\n\t\t\t\t\tJob Details: " << jobDetails << endl;
                 }
-                else if (userId.empty())
-                {
-                    pos = jobId.find("/");
-                    string jid = jobId.substr(0, pos);
-                    string compId = jobId.substr(pos + 1);
-                    string newJobId = applicantjobId + "/" + compId;
-                    if (jobId == newJobId)
-                    {
-                        cout << "\n\n\t\t\t\t\tJob ID: " << jobId << endl;
-                        cout << "\n\t\t\t\t\tJob Details: " << jobDetails << endl;
-                    }
-                }
                 else
                 {
                     pos = jobId.find("/");
@@ -200,7 +358,7 @@ protected:
     }
 
     // show all the applied jobs -> applications.txt
-    bool viewAppliedJobs(const string user_id, const string user_type)
+    bool viewAppliedJobs(const string user_id)
     {
         ifstream applicationFile("applications.txt");
         if (applicationFile.is_open())
@@ -210,18 +368,14 @@ protected:
             {
                 size_t pos = line.find("|");
                 string userId = line.substr(0, pos);
-                if (userId == user_id && user_type == "js")
+                if (userId == user_id)
                 {
                     string jobId = line.substr(pos + 1);
                     showJobs("", jobId);
                 }
-                else if (userId == user_id && user_type == "em")
-                {
-                    showJobs(user_id, "");
-                }
                 else
                 {
-                    cout << "\n\t\t\t\t\tYou have not applied for any job.";
+                    cout << "\n\t\t\t\t\tNo jobs applied.";
                     return false;
                 }
             }
@@ -500,7 +654,7 @@ public:
                      << "\t\t\t\t\t-------------------------------" << endl
                      << endl
                      << endl;
-                viewAppliedJobs(username, "js");
+                viewAppliedJobs(username);
                 cout << "\n\n\t\t\t\t\tPress Enter Key To Exit ";
                 getch();
                 break;
@@ -520,7 +674,7 @@ public:
                          << endl
                          << endl;
 
-                    if (viewAppliedJobs(username, "js"))
+                    if (viewAppliedJobs(username))
                     {
                         cin.ignore();
                         cout << "\n\t\t\t\t\tEnter The Job ID You Want To Delete: ";
@@ -558,6 +712,7 @@ public:
     }
 };
 
+// Employer Class
 class Employer : private JobApplication
 {
 private:
@@ -619,8 +774,7 @@ public:
                  << "\t\t\t\t\t3. Show Employer Profile\n"
                  << "\t\t\t\t\t4. Add New Job\n"
                  << "\t\t\t\t\t5. Show All Jobs and Vacancies\n"
-                 << "\t\t\t\t\t6. View Jobs Applied by Job Seekers\n"
-                 << "\t\t\t\t\t7. Remove Jobs\n"
+                 << "\t\t\t\t\t6. Remove Jobs\n"
                  << "\t\t\t\t\t0. Exit\n"
                  << "\n\t\t\t\t\tEnter your choice: ";
             cin >> choice;
@@ -732,25 +886,8 @@ public:
                 break;
             }
 
-            // 5. View Applied Jobs By the users
+            // 6. Remove Jobs
             case 6:
-            {
-                system("CLS");
-                cout << endl
-                     << endl
-                     << "\t\t\t\t\t-------------------------------" << endl
-                     << "\t\t\t\t\t     ::Applied Jobs::" << endl
-                     << "\t\t\t\t\t-------------------------------" << endl
-                     << endl
-                     << endl;
-                viewAppliedJobs(username, "em");
-                cout << "\n\n\t\t\t\t\tPress Enter Key To Exit ";
-                getch();
-                break;
-            }
-
-            // 7. Remove Jobs
-            case 7:
             {
                 do
                 {
@@ -801,21 +938,234 @@ public:
     }
 };
 
+// login class
+class Login
+{
+private:
+    string user, pass;
+    int choice;
+
+    // authenticate a user
+    bool authenticateUser(const string &username, const string &password, const int &reg_type)
+    {
+        string filepath;
+        string users;
+        string id;
+        string pass;
+
+        // select file path
+        switch (reg_type)
+        {
+        case 1:
+            filepath = "jobseekers_credentials.txt";
+            break;
+
+        case 2:
+            filepath = "employers_credentials.txt";
+            break;
+
+        default:
+            break;
+        }
+
+        // Try To Open The File
+        ifstream userFile;
+        userFile.open(filepath);
+
+        // check if the exists or not
+        if (userFile)
+        {
+            // Populate map with username-password pairs from the file
+            string line;
+            while (getline(userFile, line))
+            {
+                size_t pos = line.find("|");
+                id = line.substr(0, pos);
+                pass = line.substr(pos + 1);
+            }
+
+            // Check if the provided username exists and the password matches
+            if (id == username && pass == password)
+            {
+                userFile.close();
+                return true;
+            }
+            else
+            {
+                userFile.close();
+                cout << "\n\t\t\t\t\tInvalid username or password." << endl;
+                return false;
+            }
+        }
+
+        cerr << "\n\t\t\t\t\tError opening file." << endl;
+        return false;
+    }
+
+    // take input (username & password) from the user
+    bool user_login_input(int reg_type)
+    {
+        string user_type;
+        char c;
+
+        cout << "\t\t\t\t\tEnter username: ";
+        cin >> user;
+
+        cout << "\t\t\t\t\tEnter password: ";
+        cin >> pass;
+
+        bool flag = authenticateUser(user, pass, reg_type);
+
+        if (flag)
+        {
+            char c;
+            cout << "\n\t\t\t\t\tAuthentication successful!" << endl;
+            cout << "\n\t\t\t\t\tPress Enter Key To Continue ";
+            getch();
+        }
+        else
+        {
+            cout << "\n\t\t\t\t\tAuthentication failed." << endl;
+
+            switch (reg_type)
+            {
+            case 1:
+                user_type = "Job Seeker";
+                break;
+
+            case 2:
+                user_type = "Employer";
+                break;
+
+            default:
+                break;
+            }
+
+            cout << "\n\t\t\t\t\tYou are not registerd as a " << user_type << endl;
+            cout << "\n\t\t\t\t\tPlease Register as " << user_type << endl;
+            cout << "\n\t\t\t\t\tPress Enter Key To Continue ";
+            getch();
+        }
+
+        return flag;
+    }
+
+    // login as a job seeker
+    void jobseeker_login(int reg_type)
+    {
+        JobSeeker jobseeker;
+        system("CLS");
+        cout << endl
+             << endl
+             << "\t\t\t\t\t---------------------------------------" << endl
+             << "\t\t\t\t\t ::Login As A Job Seeker::" << endl
+             << "\t\t\t\t\t---------------------------------------" << endl
+             << endl;
+
+        // authenticate the job seeker
+        if (user_login_input(reg_type))
+        {
+            jobseeker.jobSeeker_func_call(user);
+        }
+    }
+
+    // login as an employer
+    void employer_login(int reg_type)
+    {
+        Employer employer;
+        system("CLS");
+        cout << endl
+             << endl
+             << "\t\t\t\t\t---------------------------------------" << endl
+             << "\t\t\t\t\t\t::Login As An Employer::" << endl
+             << "\t\t\t\t\t---------------------------------------" << endl
+             << endl;
+
+        // authenticate the employer
+        if (user_login_input(reg_type))
+        {
+            employer.employer_func_call(user);
+        }
+    }
+
+public:
+    // login call function
+    void login_call()
+    {
+        system("CLS");
+        cout << endl
+             << endl
+             << "\t\t\t\t\t----------------------------------" << endl
+             << "\t\t\t\t\t   ::Welcome to Login Screen!::" << endl
+             << "\t\t\t\t\t----------------------------------" << endl
+             << endl
+             << endl
+             << "\t\t\t\t\t1. Job Seeker Login" << endl
+             << "\t\t\t\t\t2. Employee Login" << endl
+             << "\t\t\t\t\t0. Home Screen" << endl
+             << endl
+             << "\t\t\t\t\tEnter: ";
+        cin >> choice;
+
+        // choose whom to login
+        switch (choice)
+        {
+        case 1:
+            jobseeker_login(choice);
+            break;
+
+        case 2:
+            employer_login(choice);
+            break;
+
+        default:
+            break;
+        }
+    }
+};
+
 int main()
 {
-    JobSeeker jobseeker;
-    Employer employer;
-    string username;
+    Login login;
+    Registration registration;
     int choice;
-    cout << "Enter Username: ";
-    cin >> username;
-    cout<<"\n1. Employer \n2. Job Seeker"<<endl;
-    cin>>choice;
-    if(choice==1){
-        employer.employer_func_call(username);
-    }
-    else{
-        jobseeker.jobSeeker_func_call(username);
-    }
+    do
+    {
+        system("CLS");
+        cout << endl
+             << endl
+             << "\t\t\t\t\t----------------------------------" << endl
+             << "\t\t\t\t\tWelcome To Job Application System" << endl
+             << "\t\t\t\t\t----------------------------------" << endl
+             << endl
+             << endl
+             << "\t\t\t\t\t1. Login" << endl
+             << "\t\t\t\t\t2. Register As a New User" << endl
+             << "\t\t\t\t\t0. Exit" << endl
+             << endl
+             << "\t\t\t\t\tEnter: ";
+        cin >> choice;
+        switch (choice)
+        {
+        case 1:
+            login.login_call();
+            break;
+
+        case 2:
+            registration.registration_call();
+            break;
+
+        default:
+            if (!choice == 0)
+            {
+                cout << "\n\t\t\t\t\tInvalid Input!! Please Try Again!!";
+                cout << "\n\n\t\t\t\t\tPress Enter Key To Continue ";
+                getch();
+            }
+
+            break;
+        }
+
+    } while (choice != 0);
     return 0;
 }
